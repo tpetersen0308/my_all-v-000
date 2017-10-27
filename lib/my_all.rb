ef30fa3d17236409f63112babdf09fa1ae2e96d1
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-27 18:35:39 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,13 +4,17 @@ def my_all?(collection)
   i = 0
   block_return_values = Array.new
   while i < collection.length
-    block_return_values << yield(collection[i])
-    i += 1
-  end
-
-  if block_return_values.include?(false)
-    return false
-  else
+    if yield(collection[i]) == false
+      return false
+    else
+      i += 1
+    end
     return true
   end
+
+#  if block_return_values.include?(false)
+#    return false
+#  else
+#    return true
+#  end
 end
